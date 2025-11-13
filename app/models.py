f27abf7f8b6a17ee04c8a65e3976cd80f7f7213f
lenger1117 <lenger1117@yandex.ru>
2025-11-13 14:55:03 +0300

    Создана модель Wallet

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Numeric
+from sqlalchemy.orm import Mapped, mapped_column
+from decimal import Decimal
+from .database import Base
+from uuid import UUID, uuid4
+
+class Wallet(Base):
+    __tablename__ = "wallets"
+    id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4)
+    balance: Mapped[Decimal] = mapped_column(Numeric(20, 2), nullable=False, default=0.00)
